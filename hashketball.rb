89a875a43c59117f877bf920d5f098d4b1b54288
Joshua Hill <josh@jlhworkshop.com>
2019-08-02 20:26:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -108,8 +108,8 @@ def game_hash
 end
 
 def num_points_scored(sought_player_name)
-  game_hash.each do |_place, team|
-    team.each do |attribute, data|
+  game_hash.each do |key, value|
+    value.each do |attribute, data|
       next unless attribute == :players
 
       data.each do |player|
