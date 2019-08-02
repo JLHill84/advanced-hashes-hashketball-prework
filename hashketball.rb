aeecf6e4d48336b473e76c3df5fc524667b97ae6
Joshua Hill <josh@jlhworkshop.com>
2019-08-02 21:52:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -157,7 +157,7 @@ def player_numbers(team_name)
   game_hash.each do |key, value|
     next unless value[:team_name] == team_name
     value.each do |attribute, data|
-      next unless attribute == 
+      next unless attribute == :players
       data.each do |data|
         numbers << data[:number]
       end
